9ef96fb576a52ed503ad3dac48dce5534330f2ef
Hitalio <juancarosella96@gmail.com>
2022-09-13 17:22:08 -0300

    added get function

--- index.js
+++ index.js
@@ -4,22 +4,31 @@ import { concat as uint8ArrayConcat } from 'uint8arrays/concat'
 import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
 import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
 
-async function main () {
-  const node = await IPFS.create()
-  const version = await node.version()
+async function test_node() {
+  // Create ipfs_node
+  const ipfs_node = await IPFS.create()
 
+  // Log version
+  const version = await ipfs_node.version()
   console.log('Version:', version.version)
 
-  const file = await node.add({
-    path: 'hello.txt',
-    content: uint8ArrayFromString('Hello World 101')
+  // Add file with ipfs add
+  const file = await ipfs_node.add({
+    path: 'hello_world.txt',
+    content: uint8ArrayFromString('Hello World!')
   })
+  console.log('Added file:', file.path, "with cid:", file.cid.toString())
 
-  console.log('Added file:', file.path, file.cid.toString())
-
-  const data = uint8ArrayConcat(await all(node.cat(file.cid)))
-
-  console.log('Added file contents:', uint8ArrayToString(data))
+  // Read file with ipfs cat
+  const data = uint8ArrayConcat(await all(ipfs_node.cat(file.cid)))
+  console.log('File contents:', uint8ArrayToString(data))
 }
 
-main()
+async function get_cid(cid) {
+  const dir = await all(ipfs_node.get(cid))
+
+  console.log("Contents in directory:")
+  console.log(dir)
+}
+
+test_node()
